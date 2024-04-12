4b60a12920fee8bfcaafd73815355c7417983b93
Ashu <ashuprasad1227@gmail.com>
2024-04-12 23:06:33 +0530

    connection with api with the help of api key estiblished succesfully and all the required data is fetching properly. all working good no error

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,17 @@ import Inputs from './components/Inputs'
 import TimeAndLocation from './components/TimeAndLocation'
 import TempuratureAndDetails from './components/TempuratureAndDetails'
 import Forecast from './components/Forecast'
+import getFormatedWeatherData from './services/weatherService'
+
+
 
 function App() {
-
+const fetchWeather = async () =>{
+  const data = await getFormatedWeatherData({q:"london"});
+  console.log(data);
+ 
+}
+fetchWeather();
 
   return (
     <div className='mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400'>
--- src/components/TopButtons.jsx
+++ src/components/TopButtons.jsx
@@ -27,7 +27,7 @@ function TopButtons() {
     <div className='flex items-center justify-around my-6'>
         {cities.map((city) => (
         
-            <button className='text-white text-lg font-medium'>{city.title}</button>
+            <button  key={city.id} className='text-white text-lg font-medium'>{city.title}</button>
         ))}
       
     </div>
